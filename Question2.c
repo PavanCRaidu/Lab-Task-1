52b348a9f1fc53316c489c403ddbc87004d66db9
PavanCRaidu <pavaneluri100@gmail.com>
2025-01-13 18:01:53 +0530

    Create Question2.c

---
+++ Question2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h> 
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0;
+    char *ptr;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    ptr = str;
+    while (*ptr != '\0') {
+        char ch = tolower(*ptr);
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+        ptr++;
+    }
+    printf("Number of vowels: %d\n", vowels);
+    printf("Number of consonants: %d\n", consonants);
+    return 0;
+}
