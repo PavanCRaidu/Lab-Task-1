c7e7d915da398a2600a7eb323dd1cc6be0f9d133
PavanCRaidu <pavaneluri100@gmail.com>
2025-01-13 18:05:47 +0530

    Create Question5.c

---
+++ Question5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    char *ptr;
+    int length = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    ptr = str;
+    while (*ptr != '\0') {
+        if (*ptr == '\n') {
+            break;
+        }
+        length++;
+        ptr++;
+    }
+    printf("The length of the string is: %d\n", length);
+    return 0;
+}
